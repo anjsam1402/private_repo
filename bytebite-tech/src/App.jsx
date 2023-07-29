fc6b29197cd7b31e0eb73e8161b716279c2fa999
AnjaliSamad <samad.anjali.14@gmail.com>
2023-07-29 17:37:09 +0200

    NULL

---
+++ bytebite-tech/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="main_div">
+        <h1>Welcome to ByteBite Tech!</h1>
+        <a href="https://www.youtube.com/@ByteBite_Tech" target="_blank">
+          <img src="channels4_profile.jpeg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
